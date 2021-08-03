ee467e0f078e911d84299842ffafbc054dc0a4ec
darklord1279 <88158468+darklord1279@users.noreply.github.com>
2021-08-03 18:56:12 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -288,9 +288,9 @@ except KeyError:
 try:
     IMAGE_URL = getConfig('IMAGE_URL')
     if len(IMAGE_URL) == 0:
-        IMAGE_URL = 'https://imagetot.com/images/2021/06/28/e8155fe3d87a1084d7cda5f4ecc5211c.jpg'
+        IMAGE_URL = 'https://wallpapercave.com/wp/wp2475724.png'
 except KeyError:
-    IMAGE_URL = 'https://imagetot.com/images/2021/06/28/e8155fe3d87a1084d7cda5f4ecc5211c.jpg'
+    IMAGE_URL = 'https://wallpapercave.com/wp/wp2475724.png'
 
 IGNORE_PENDING_REQUESTS = False
 try:
